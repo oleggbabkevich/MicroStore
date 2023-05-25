15a0c0b56b45854cc447a2707190341523dcac64
Oleg Babkevich <olegbabkevich@coherentsolutions.com>
2023-05-25 15:56:26 +0200

    update

---
+++ UserMS/main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=3.0.0"
+    }
+  }
+}
+
+terraform {
+    backend "azurerm" {
+        resource_group_name  = "tf_rg_blobstorage"
+        storage_account_name = "tfolegbabkevichstorage"
+        container_name       = "tfstate"
+        key                  = "terraform.tfstate"
+    }
+}
+
+provider "azurerm" {
+    features {}
+}
+
+resource "azurerm_resource_group" "tf_test"{
+    name = "tfmainrg"
+    location = "East US"
+}
+
+resource "azurerm_container_group" "tfcg_test"{
+    name = "userms"
+    location = azurerm_resource_group.tf_test.location
+    resource_group_name = azurerm_resource_group.tf_test.name
+    ip_address_type = "Public"
+    dns_name_label = "olegbabkevichusermicroservice"
+    os_type= "Linux"
+
+    container {
+    name            = "userms"
+    image           = "olegbabkevichcoherentsolutions/usermicroservice"
+    cpu             = "1"
+    memory          = "1"
+
+    ports {
+        port        = 80
+        protocol    = "TCP"
+    }
+  }
+}
+
